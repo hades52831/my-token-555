0ce3065ef7dc41fea0b65699c2b40935b20e15e2
Haded5 <95569570+hades52831@users.noreply.github.com>
2023-07-20 12:33:50 +0330

    Update index.js
    update con

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0x753b6cb32ded0bd87a04ed2702d419b626efd533";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
